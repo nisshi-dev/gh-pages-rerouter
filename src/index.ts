19bd7f6b2f923d17ca549180d596cfe286342bec
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-05-24 02:27:15 +0900

    :sparkles: added reverse proxy

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,19 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler deploy src/index.ts --name my-worker` to deploy your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request))
+})
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+async function handleRequest(request: Request): Promise<Response> {
+  const url = new URL(request.url)
+
+  if (url.pathname.endsWith('/')) {
+    url.pathname = url.pathname.slice(0, url.pathname.length - 1)
+  }
+
+  if (url.pathname.startsWith('/prototypes/')) {
+    url.pathname = url.pathname.replace('/prototypes/', '/')
+  }
+
+  const targetURL = 'https://nisshi-dev.github.io' + url.pathname
+
+  return fetch(targetURL)
 }
-
-export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
-};
