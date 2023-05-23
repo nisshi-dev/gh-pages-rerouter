33e5fe84fc6d759553567e508f9d4174892a83dd
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-05-24 05:17:23 +0900

    :wrench: Add eslint and prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-addEventListener('fetch', (event: FetchEvent) => {
-  event.respondWith(handleRequest(event.request))
-})
+addEventListener("fetch", (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request: Request): Promise<Response> {
-  const url = new URL(request.url)
+  const url = new URL(request.url);
 
-  if (url.pathname.endsWith('/')) {
-    url.pathname = url.pathname.slice(0, url.pathname.length - 1)
+  if (url.pathname.endsWith("/")) {
+    url.pathname = url.pathname.slice(0, url.pathname.length - 1);
   }
 
-  if (url.pathname.startsWith('/prototypes/')) {
-    url.pathname = url.pathname.replace('/prototypes/', '/')
+  if (url.pathname.startsWith("/prototypes/")) {
+    url.pathname = url.pathname.replace("/prototypes/", "/");
   }
 
-  const targetURL = 'https://nisshi-dev.github.io' + url.pathname
+  const targetURL = "https://nisshi-dev.github.io" + url.pathname;
 
-  return fetch(targetURL)
+  return fetch(targetURL);
 }
